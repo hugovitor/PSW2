eee3470bef3874878f6b17c376155de3b0929376
hugovitor <hugovitormnunes@gmail.com>
2017-02-16 20:40:17 -0200

    primeira versão

---
+++ AulaExercicioHibernate/src/java/Ocorrencia.java
@@ -0,0 +1,66 @@
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ra21503801
+ */
+@Entity
+public class Ocorrencia {
+    private String nome;
+    private String endereco;
+    private String tipo_ocorrencia;
+    private int idade;
+    
+    @Id
+    private int id;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTipo_ocorrencia() {
+        return tipo_ocorrencia;
+    }
+
+    public void setTipo_ocorrencia(String tipo_ocorrencia) {
+        this.tipo_ocorrencia = tipo_ocorrencia;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+}
---
+++ AulaExercicioHibernate/src/java/SalvarOcorrencia.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author ra21503801
+ */
+public class SalvarOcorrencia extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            
+            Ocorrencia o1 = new Ocorrencia();
+            o1.setNome("Hugo");
+            o1.setEndereco("C.A.S");
+            o1.setTipo_ocorrencia("Transito");
+            o1.setId(01);
+            
+            Session sessao = HibernateUtil.getSessionFactory().openSession();
+            
+            Transaction tx = sessao.beginTransaction();
+            
+            sessao.save(o1);
+            sessao.flush();
+            
+            tx.commit();
+            
+            sessao.close();
+            
+            
+            
+            
+            
+            
+            
+            
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet SalvarOcorrencia</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet SalvarOcorrencia at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+              
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
